602629fcc44f9f1b723a232afc3f8bd82a183fa3
Mrudhul P <mrudhulp212@gmail.com>
2026-03-03 11:50:38 +0530

    db connected

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-const express = require('express')
-const app = express()
-const port = 3000
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+dotenv.config();
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+const DB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+
+    console.log("DB connected Successfully");
+  } catch (error) {
+    console.log("DB connection error:", error);
+  }
+};
+DB();
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
